9e366ef2b5d63a5b839b2e4f87577961f2837e35
gofna <gofnaivry90@gmail.com>
2020-03-23 16:45:20 +0200

    fix find

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -6,7 +6,18 @@ using namespace std;
 namespace phonetic{
 
 string find(string text, string w){
+    
     size_t found =  text.find(w);
+    string temp = "";
+    int count = 0;
+    for (int i = found ; text[i] != ' ' && i < text.length() ; i++){
+        count++;
+    }
+
+    if(count > w.length()){
+        throw runtime_error("'"+w+"'" + " is not a full word in the sentence");
+    }
+    
     if (found != -1){
         return w;
     }
